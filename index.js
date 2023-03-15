964d7d15828964349376c874d978f326d5091f6e
pajagl <114282176+pajagl@users.noreply.github.com>
2023-03-15 22:00:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log('funguju!');
+
+const alarm=document.querySelector('.alarm')
+const audio=document.querySelector('audio')
+const zruseniZvoneni=document.querySelector('.zruseniZvoneni')
+const zadaniUzivatele=Number(prompt('zadej cas zvoneni'))
+
+const zvoneni = setTimeout(() => {
+    alarm.classList.add('alarm--ring')
+    audio.play()
+},zadaniUzivatele*1000)
+
+zruseniZvoneni.addEventListener('click', () => {
+    audio.pause()
+    clearTimeout(zvoneni)
+})
